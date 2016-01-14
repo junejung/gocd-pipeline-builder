5feb47c7c5ca2dbfb67c57a39cc5a5c19f01c6e2
June Jung <jjung@thoughtworks.com>
2016-01-13 19:32:34 -0800

    June/Piyush - added yaml loader to read the all the inventory information

--- populate_template.py
+++ populate_template.py
@@ -1,7 +1,12 @@
 from jinja2 import Template
+import yaml
 import os
+
+inventory_file = open('inventory.yaml')
+inventories = yaml.load(inventory_file)
+
 
 with open(os.path.join('templates', 'demo_pipeline.json')) as template_file:
     template_json = template_file.read()
     template = Template(template_json)
-    print template.render( INVENTORY='')
+    print template.render( INVENTORY = inventories['pipelines'][0]['portfolio']['inventory'][0]['repository']['url'] )
