4f459d96e3a4fc668c886a9cf78463c9eeb5a555
June Jung <jjung@thoughtworks.com>
2016-01-13 18:13:48 -0800

    June/Piyush - replaced pipeline templet with the request version json

--- populate_template.py
+++ populate_template.py
@@ -4,5 +4,4 @@ import os
 with open(os.path.join('templates', 'demo_pipeline.json')) as template_file:
     template_json = template_file.read()
     template = Template(template_json)
-    print template.render(GO_SERVER_URL='localhost:8153',
-            PIPELINE_NAME='DEMO_PIPELINE')
+    print template.render( INVENTORY='')
