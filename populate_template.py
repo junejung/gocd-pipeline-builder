909aea48f69bf50825cde68ecbfd7c86010f3487
June Jung <jjung@thoughtworks.com>
2016-01-13 19:49:13 -0800

    June/Piyush - added loop to create multiple json

--- populate_template.py
+++ populate_template.py
@@ -6,7 +6,11 @@ inventory_file = open('inventory.yaml')
 inventories = yaml.load(inventory_file)
 
 
-with open(os.path.join('templates', 'demo_pipeline.json')) as template_file:
-    template_json = template_file.read()
-    template = Template(template_json)
-    print template.render( INVENTORY = inventories['pipelines'][0]['portfolio']['inventory'][0]['repository']['url'] )
+for portfolio in inventories['pipelines']:
+    with open(os.path.join('templates', 'demo_pipeline.json')) as template_file:
+        template_json = template_file.read()
+        template = Template(template_json)
+        for repository in portfolio['portfolio']['inventory']:
+            print template.render( PORTFOLIO = portfolio['portfolio']['name'], 
+        	    INVENTORY_NAME = repository['repository']['name'], 
+        	    INVENTORY = repository['repository']['url'] )
